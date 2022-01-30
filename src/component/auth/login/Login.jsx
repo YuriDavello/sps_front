762c91199f8ab363264263f2c5ff568fd2653f33
yuridavello <yuridavello@hotmail.com>
2022-01-30 01:08:50 -0300

    handling user's register data

--- src/component/auth/login/Login.jsx
+++ src/component/auth/login/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
         window.sessionStorage.setItem("token", "iruypayloadmeupau");
         navigate('/app');
     }
-    
+
     return(
         <div className="container">
             <form className="form">
--- src/component/auth/register/Register.jsx
+++ src/component/auth/register/Register.jsx
@@ -1,45 +1,54 @@
+import { User } from '../../model/user/User';
+import { useState } from 'react';
 import './Register.css';
 
 function Register(){
+    const [nameText, setNameText] = useState('');
+    const [emailText, setEmailText] = useState('');
+    const [passwordText, setPasswordText] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const user = new User(nameText, emailText, passwordText);
+        console.log(user);
+    }
+
+    const handleNameChange = (event) => {
+        const nameTextInput = event.target.value;
+        setNameText(nameTextInput);
+    }
+
+    const handleEmailChange = (event) => {
+        const emailTextInput = event.target.value;
+        setEmailText(emailTextInput);
+    }
+
+    const handlePasswordChange = (event) => {
+        const passwordTextInput = event.target.value;
+        setPasswordText(passwordTextInput);
+    }
+
     return(
         <div className="container">
-            <form className="register-form">
+            <form className="register-form" onSubmit={handleSubmit}>
                 <h3>Cadastro</h3>
+
                 <div className="form-group">
                     <label htmlFor="exampleInputNome">Nome</label>
-                    <input
-                        type="text" 
-                        className="form-control" 
-                        id="exampleInputNome" 
-                        aria-describedby="emailHelp" 
-                        placeholder="Name" 
-                        name="name"
-                    />
+                    <input onChange={handleNameChange} type="text" placeholder="Name" />
                 </div>
+
                 <div className="form-group">
-                <label htmlFor="exampleInputEmail1">E-mail</label>
-                <input
-                    type="email" 
-                    className="form-control" 
-                    id="exampleInputEmail1" 
-                    aria-describedby="emailHelp" 
-                    placeholder="Email" 
-                    name="email"
-                />
+                    <label>E-mail</label>
+                    <input onChange={handleEmailChange} type="email" placeholder="Email" />
                 </div>
+
                 <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Senha</label>
-                <input
-                    type="password" 
-                    className="form-control" 
-                    id="exampleInputPassword1" 
-                    placeholder="Password" 
-                    name="password"
-                />
+                    <label>Senha</label>
+                    <input onChange={handlePasswordChange} type="password" placeholder="Password" />
                 </div>
-                <div className="form-group-question">
-                    <small>Já tem uma conta ?</small>
-                </div>
+
+                <div className="form-group-question"><small>Já tem uma conta ?</small></div>
                 {/* {
                     isLoading ?
                     <img
@@ -48,7 +57,7 @@ function Register(){
                     /> : 
                     <button className="btn" type="submit">Criar conta</button>
                 } */}
-                <button>Criar conta</button>
+                <button type="submit">Criar conta</button>
             </form>
         </div>
     );
